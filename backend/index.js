6f3792de53c3741f6eb4c4d087af124262a3040d
Muhammad Munir <mmunir1214@gmail.com>
2024-09-01 17:00:54 -0400

    Make URL Queries to Test Response

--- backend/index.js
+++ backend/index.js
@@ -6,4 +6,12 @@ app.get("/test", (req, res) => {
   return res.json("testing");
 });
 
+app.get("/create-story", (req, res) => {
+  const url = req.query.url;
+  console.log({
+    url,
+  });
+  return res.json("okay");
+});
+
 app.listen(8080, () => console.log("listening on port 8080"));
