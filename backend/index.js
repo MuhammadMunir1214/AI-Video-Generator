4fcfe7b6defbce0d28645ef22f94bfbe15f73914
Muhammad Munir <mmunir1214@gmail.com>
2024-09-05 17:56:20 -0400

    Fixed Dependencies Issues

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,40 @@
 import express from "express";
+import uniqid from "uniqid";
+import fs from "fs";
+import cors from "cors";
+import { GPTScript, RunEventType } from "@gptscript-ai/gptscript";
+
+const gptscript = new GPTScript();
 
 const app = express();
+app.use(cors());
 
 app.get("/test", (req, res) => {
   return res.json("testing");
 });
 
-app.get("/create-story", (req, res) => {
+app.get("/create-story", async (req, res) => {
   const url = req.query.url;
+  const dir = uniqid();
+  const path = "./stories/" + dir;
+  fs.mkdirSync(path, { recursive: true });
+
   console.log({
     url,
   });
-  return res.json("okay");
+
+  const run = await gptscript.run("./story.gpt", {
+    input: "--url ${url} --dir ${dir}",
+    disableCache: true,
+  });
+  run.on(RunEventType.Event, (ev) => {
+    if (ev.type === RunEventType.CallFinish) {
+      console.log(ev.output);
+    }
+  });
+  const result = await run.text();
+
+  return res.json(result);
 });
 
 app.listen(8080, () => console.log("listening on port 8080"));
